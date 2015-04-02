da55a6c6ca80a45ec7f9d5c495b17da8170f0e0b
TAKEDA hiroyuki(aka 3ign0n) <take6a.hir0yuki@gmail.com>
2015-04-03 00:28:17 +0900

    add project files

---
+++ MOAspectsSwiftExapmple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MOAspectsSwiftExapmple
+//
+//  Created by 3ign0n on 2015/04/02.
+//  Copyright (c) 2015年 3ign0n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
